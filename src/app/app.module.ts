289f4674b377028b58b112a09726af0804b66f59
alexr1708 <ing.alex.reyes931230@outlook.com>
2023-12-28 14:35:37 -0600

    28/12/2023 * add form module into app module * update cargo roder html:     - add search function     - show order data * add search funtions in cargo order component:     - add getDate funtion     - add get Hour function     - add get Time function     - add get route Tracking     - get search function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CargoOrdersComponent } from './components/cargo-orders/cargo-orders.component';
@@ -14,7 +15,8 @@ import { CargoOrdersComponent } from './components/cargo-orders/cargo-orders.com
   imports: [
     BrowserModule,
     routing,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     appRoutingProviders,
