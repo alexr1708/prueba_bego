a927b4c8791b005a97ede8d652a5b2cd41187bca
alexr1708 <ing.alex.reyes931230@outlook.com>
2023-12-28 01:33:17 -0600

    27/12/2023 * update tsconfig.json * add new routes into routing module * update cargo orders components:     -  add 2 classes on css     - add get function on component.ts     - add card view in html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Route, RouterModule, Routes } from '@angular/router';
 
 import { CargoOrdersComponent } from './components/cargo-orders/cargo-orders.component';
 import { CargoDetailsComponent } from './components/cargo-details/cargo-details.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
-  { path: '', component: CargoOrdersComponent, title: 'Cargo Orders' }, // defined a title prop
-  { path: 'cargos/:id', component: CargoDetailsComponent, title: 'Detail'},
+  { path: '', component: HomeComponent, title: 'Home' }, // defined a title prop
+  { path: 'orders', component: CargoOrdersComponent, title: 'Cargo Orders' }, // defined a title prop
+  { path: 'orders/:id', component: CargoDetailsComponent, title: 'Detail'},
+  { path: '**', component: ErrorComponent, title: '404 Not Found'},
 ];
 
 export const appRoutingProviders: any[] = [];
--- src/app/components/cargo-orders/cargo-orders.component.ts
+++ src/app/components/cargo-orders/cargo-orders.component.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
+import { order } from '../../models/order';
+import { OrderService } from '../../services/orders.service';
 
 @Component({
   selector: 'cargo-orders',
   templateUrl: './cargo-orders.component.html',
-  styleUrl: './cargo-orders.component.css'
+  styleUrl: './cargo-orders.component.css',
+  providers: [
+    OrderService
+  ]
 })
 
 export class CargoOrdersComponent {
 
-  constructor(){}
+  public orders:order[] = [];
+
+  constructor(
+    private _orderService: OrderService
+  ){}
 
   ngOnInit(){
+    this.getOrders()
+  }
 
+  getOrders(){
+    this._orderService.getOrders().subscribe(
+      response => {
+        if(response.status = 200){
+          this.orders = response.result;
+          console.table(this.orders)
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 }
