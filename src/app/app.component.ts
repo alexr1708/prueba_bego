09d32fc6edb79fbb71e66bc12565f7e3fe12a9cb
alexr1708 <ing.alex.reyes931230@outlook.com>
2023-12-27 21:30:08 -0600

    27_12_2023 * modify main component.ts * modify index.html     - add body class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prueba_bego';
+  constructor(
+    private router: Router,
+    private titleService: Title,
+    private route: ActivatedRoute
+  ){ }
+
+  ngOnInit():void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          const child: ActivatedRoute | null = this.route.firstChild;
+          let title = child && child.snapshot.data['title'];
+          if (title) {
+            return title;
+          }
+        })
+      )
+      .subscribe((title) => {
+        if (title) {
+          this.titleService.setTitle(`App Prefix - ${title}`);
+        }
+      });
+  }
 }
